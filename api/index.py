54711870088a00a8e1c585f091dbb79c6f993248
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-08-12 23:34:58 +0530

    f

--- api/index.py
+++ api/index.py
@@ -44,9 +44,9 @@ if __name__ == '__main__':
 # from flask import Flask
 # app = Flask(__name__)
 
-# @app.route('/api/hello', methods=['GET'])
-# def hello_world():
-#     return "Hello, World!"
+@app.route('/api/hello', methods=['GET'])
+def hello_world():
+    return "Hello, World!"
 
-# if __name__ == '__main__':
-#     app.run(port=5328)
+if __name__ == '__main__':
+    app.run(port=5328)
