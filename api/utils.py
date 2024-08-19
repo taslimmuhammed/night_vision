6ff18e8f0c930991c7b4cec7f2590ebe1ddd3984
taslimmuhammed <taslimmuhammed67@gmail.com>
2024-08-19 21:07:56 +0530

    m

--- api/utils.py
+++ api/utils.py
@@ -1,44 +1,48 @@
-from PIL import Image
 import numpy as np
-import tflite_runtime.interpreter as tflite
+from PIL import Image
+import tensorflow as tf
 
-_interpreter = None
+_model = None
 
 def load_model():
-    print("Loading Model------------------")
-    global _interpreter
-    model_path = "model.tflite"
-    _interpreter = tflite.Interpreter(model_path=model_path)
-    _interpreter.allocate_tensors()
-    print("Model successfully loaded")
+    global _model
+    model_path = 'model.tflite'  # Path to the TFLite model file
+    _model = tf.lite.Interpreter(model_path=model_path)
+    _model.allocate_tensors()
+    print("Model successfully loaded and running")
 
 def preprocess_image(image):
-    original_shape = image.shape
-    resized_img = Image.fromarray(image).resize((500, 500))
+    original_shape = image.size  # PIL images use (width, height) format
+    resized_img = image.resize((500, 500))  # Resize image to (500, 500)
     formatted_img = np.expand_dims(np.array(resized_img), axis=0)
-    return original_shape, formatted_img.astype(np.float32)
+    return original_shape, formatted_img
 
 def get_prediction(image):
-    global _interpreter
-    if _interpreter is None:
+    global _model
+    if _model is None:
         load_model()
     
-    input_details = _interpreter.get_input_details()
-    output_details = _interpreter.get_output_details()
+    input_details = _model.get_input_details()
+    output_details = _model.get_output_details()
     
-    _interpreter.set_tensor(input_details[0]['index'], image)
-    _interpreter.invoke()
-    prediction = _interpreter.get_tensor(output_details[0]['index'])
-    return prediction[0]
+    _model.set_tensor(input_details[0]['index'], image.astype(np.float32))
+    _model.invoke()
+    
+    prediction = _model.get_tensor(output_details[0]['index'])
+    predicted_img = prediction[0]
+    return predicted_img
 
 def post_process_image(predicted_img, original_shape):
-    original_size_img = Image.fromarray(predicted_img.astype(np.uint8)).resize((original_shape[1], original_shape[0]))
-    return np.array(original_size_img.convert('RGB'))
+    predicted_img = np.clip(predicted_img, 0, 255)  # Ensure pixel values are within valid range
+    predicted_img = predicted_img.astype(np.uint8)
+    
+    resized_img = Image.fromarray(predicted_img)
+    original_size_img = resized_img.resize(original_shape, Image.ANTIALIAS)
+    
+    return original_size_img
 
 def convert_image(image):
     shape, image = preprocess_image(image)
     prediction = get_prediction(image)
     rgb_img = post_process_image(prediction, shape)
     return rgb_img
-
-    
